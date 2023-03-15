9b9a04522e7aa5092e0581ba1bcca6bc60bb4adf
boom <ranjit.makwana@gmail.com>
2023-03-15 11:38:43 +0000

    updated footer for media icons to work in a responsive way

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,23 +4,19 @@ import './style.css';
 function Footer() {
   return (
     // <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
-    <div className="icons">
-
-        <a href="https://github.com/ranjmak">
-            <i className="fa-brands fa-github fa-lg fa-fw" aria-hidden="true"></i>
-        </a>
-
-
-        <a href="https://www.linkedin.com/in/ranjit-makwana/">
-            <i className="fa-brands fa-linkedin fa-lg fa-fw" aria-hidden="true"></i>
-        </a>
-
-        {/* <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="ranjit-makwana-2332511" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://uk.linkedin.com/in/ranjit-makwana-2332511?trk=profile-badge">Ranjit Makwana</a></div> */}
-
-        <a href="https://twitter.com/ranjimak">
-            <i className="fa-brands fa-twitter fa-lg fa-fw" aria-hidden="true"></i>
-        </a>
-
+    <div className='row'>
+        <div className="icons col-12">
+            <a href="https://github.com/ranjmak">
+                <i className="fa-brands fa-github fa-lg fa-fw" aria-hidden="true"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/ranjit-makwana/">
+                <i className="fa-brands fa-linkedin fa-lg fa-fw" aria-hidden="true"></i>
+            </a>
+            {/* <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="ranjit-makwana-2332511" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://uk.linkedin.com/in/ranjit-makwana-2332511?trk=profile-badge">Ranjit Makwana</a></div> */}
+            <a href="https://twitter.com/ranjimak">
+                <i className="fa-brands fa-twitter fa-lg fa-fw" aria-hidden="true"></i>
+            </a>
+        </div>
     </div>
   );
 }
